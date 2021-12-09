36c027e93f91b9cff1c387d0d7e190658d2d1cb8
Adrian Sanchis Gallego <adrian@devaway.es>
2021-12-09 15:32:53 +0100

    refactor caller to lazy load

--- front/infra/ui/react/hooks/useCaller.ts
+++ front/infra/ui/react/hooks/useCaller.ts
@@ -1,34 +1,26 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface HookModel<T>{
   loading: boolean
   error: any
   data: T | undefined
-  fetch: () => void
 }
-export function useCaller<Response> (promiseFn: () => Promise<Response>, callOnload = false): HookModel<Response> {
+export function useCaller<Response, ParamType = any> (promiseFn: (params?: ParamType) => Promise<Response>, params?: ParamType): HookModel<Response> {
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState<Response>()
   const [error, setError] = useState()
 
-  const callFunction = useCallback(() => {
+  useEffect(() => {
     setLoading(true)
-    promiseFn()
+    promiseFn(params)
       .then(response => setData(response))
       .catch(error => setError(error))
       .finally(() => setLoading(false))
   }, [])
 
-  useEffect(() => {
-    if (callOnload) {
-      callFunction()
-    }
-  }, [])
-
   return {
     loading,
     data,
-    fetch: callFunction,
     error
   }
 }
---
+++ front/infra/ui/react/hooks/useLazyCaller.ts
@@ -0,0 +1,34 @@
+import { useCallback, useState } from 'react'
+
+interface HookModel<Response, ParamsType>{
+  loading: boolean
+  error: any
+  run: (params: ParamsType) => Promise<Response>
+  data: Response | undefined
+}
+export function useLazyCaller<Response, ParamType = any> (promiseFn: (params: ParamType) => Promise<Response>): HookModel<Response, ParamType> {
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<Response>()
+  const [error, setError] = useState()
+
+  const run = useCallback(async (inputParams: ParamType) => {
+    setLoading(true)
+    return await promiseFn(inputParams)
+      .then(response => {
+        setData(response)
+        return response
+      })
+      .catch(error => {
+        setError(error)
+        throw error
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return {
+    loading,
+    data,
+    run,
+    error
+  }
+}
