0fd1330e8db8c4b08e0f9ae2789bb40ca93c493d
Adrian Sanchis Gallego <adrian@devaway.es>
2021-12-09 15:37:46 +0100

    chore: remove unused imports

--- front/infra/ui/react/components/ShopDetail/components/Burger/components/ModalCreateMark/index.tsx
+++ front/infra/ui/react/components/ShopDetail/components/Burger/components/ModalCreateMark/index.tsx
@@ -1,15 +1,6 @@
 import { Burger } from 'front/domain/entities/Burger'
-import { FormEvent, ReactElement, useState } from 'react'
+import { ReactElement } from 'react'
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/button'
-import { Textarea } from '@chakra-ui/textarea'
-import { Image } from '@chakra-ui/image'
-import { Center, Divider, Heading, Text } from '@chakra-ui/layout'
-import { Spinner } from '@chakra-ui/spinner'
-import { MdAdd, MdHorizontalRule } from 'react-icons/md'
-import { MarkUseCase } from 'front/usecases'
-import { CreateMark, Mark } from 'front/domain/entities/Mark'
-import { useLazyCaller } from 'front/infra/ui/react/hooks/useLazyCaller'
 import CreateMarkForm from 'front/infra/ui/react/components/CreateMarkForm'
 
 interface Props{
